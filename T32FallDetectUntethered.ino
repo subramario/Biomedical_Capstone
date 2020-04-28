03b5820457daee892e768ba40c5531eead310a09
a7subram <a7subram@uwaterloo.ca>
2020-04-28 13:19:19 -0400

    Refactored

--- T32FallDetectUntethered.ino
+++ T32FallDetectUntethered.ino
@@ -42,8 +42,7 @@ const int buttonPin = 2; //start button on the jacket --> starts the program
 // Indicator pins
 const int indicatorRedPin = 3;
 const int indicatorGreenPin = 4;
-// Actuator pin (currently solenoid)
-const int actuatorPin = 5;
+const int actuatorPin = 5; // Actuator pin (connected to solenoid)
 
 // Time counters for measurement duration (in microseconds)
 unsigned long measureDuration = 20000000; // 20 seconds
@@ -60,6 +59,7 @@ bool trigger_flag = false;
 
 //SETUP LOOP 
 void setup() {
+
   // serial to display data
   /*
   Serial.begin(230400);
@@ -67,23 +67,28 @@ void setup() {
   */
 
   // Initialize button pin, actuator LED and indicator LED
-  //pinMode(LED_BUILTIN,OUTPUT);
+  // Format: pinMode(LED_BUILTIN,OUTPUT);
   pinMode(buttonPin,INPUT);
   pinMode(indicatorRedPin,OUTPUT);
   pinMode(indicatorGreenPin,OUTPUT);
   pinMode(actuatorPin,OUTPUT);
 
-  // start communication with IMU 
-  status = IMU.begin();
-  //Error Checking for computer connected testing
+  
+  status = IMU.begin(); // start communication with IMU 
+  
+  //Error Checking for failed IMU connection
   if (status < 0) { 
+
+    // Error output for computer connected testing
     /*
     Serial.println("IMU initialization unsuccessful");
     Serial.println("Check IMU wiring or try cycling power");
     Serial.print("Status: ");
     Serial.println(status);
     */
-    while(1) { //Literally for blinking 
+
+   //Red LED will blink to indicate error
+    while(1) { 
       digitalWrite(indicatorRedPin, HIGH);
       delay(250);
       digitalWrite(indicatorRedPin, LOW);
@@ -91,23 +96,22 @@ void setup() {
       }
   }
 
-  // must setup accelerometer sensitivity ranges --> acquired values using Akshit falling 50 times LOL
-  // setting the accelerometer full scale range to +/-8G 
-  IMU.setAccelRange(MPU9250::ACCEL_RANGE_8G);
-  // setting the gyroscope full scale range to +/-500 deg/s
-  IMU.setGyroRange(MPU9250::GYRO_RANGE_500DPS);
+  // Applying sensitivity ranges--> acquired ranges using 50 trials of controlled fall testing
+  IMU.setAccelRange(MPU9250::ACCEL_RANGE_8G); // setting the accelerometer full scale sensitivity range to +/-8G
+  IMU.setGyroRange(MPU9250::GYRO_RANGE_500DPS); // setting the gyroscope full scale sensitivity range to +/-500 deg/s
+
   /*
   NOTE: Data Output Rate = 1000 / (1+SRD) --> implicit formula within accelerometer libraries
   Data Output Rate and Data Collection/Read rate should be > 2x DLPF (Digital Low Pass Filter) bandwidth
   Implicit noise filtering --> filters out any noise above a certain volume
   */
-  // setting DLPF bandwidth to 184 Hz
-  IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_184HZ);
-  // setting SRD to 1 for a 500 Hz update rate
-  IMU.setSrd(1);
+
+  IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_184HZ); // setting DLPF bandwidth to 184 Hz
+  IMU.setSrd(1); // setting SRD to 1 for a 500 Hz update rate
+
 
   // Apply biases and scale factors to accelerometer --> shifts and scales accelerometer readings based on hardware you are using 
-  // Values derived based on a calibration test
+  // Values derived based on flat surface calibration test (CalibrateCheck.ino)
   IMU.setAccelCalX(0.05,1.00);
   IMU.setAccelCalY(0.16,1.00);
   IMU.setAccelCalZ(0.39,0.99);
@@ -120,37 +124,42 @@ void setup() {
 
 //MAIN LOOP
 void loop() {
+  // Variables which store the current reading at any point in time
   float current_X_accel;
   float current_Y_gyro;
-  float threshold_X_accel;
-  float threshold_Y_gyro;
+
+  // Initializing variables to store average values of both accelerometer and gyrometer buffers
+  float threshold_X_accel = 0;
+  float threshold_Y_gyro = 0;
+
+  // Limit markers which govern whether a fall is occuring --> values determined through experimental testing
+  float threshold_limit_X_accel = -4;
+  float threshold_limit_Y_gyro = -1;
   
   // Set flag and timer on button press
   if(digitalRead(buttonPin) == HIGH){
     sensor_flag = true;
-    startTime = micros();
+    startTime = micros(); // Returns the number of microseconds since the Arduino board began running the current program
 
-  // Indicate LEDs accordingly
+    // Indicate LEDs accordingly
     digitalWrite(indicatorRedPin, LOW);
     digitalWrite(actuatorPin, LOW);
   }
 
-  // Start timer once sensor flag is set
-  // Basically: Am I going to make measurements during this loop? 
+  // Start timer once sensor flag is set --> Basically: Am I going to make measurements during this loop? 
   if(sensor_flag == true){
-    // Start timer
-    currentTime = micros();
+    currentTime = micros(); // Start timer
     deltaTime = currentTime - startTime; //Time between when we push button and the start of this instance of the loop
 
-    // If deltatime is less than 20 seconds    
+    // Runs if deltatime is less than 20 seconds    
     if (deltaTime < measureDuration){
-      // Read the sensor
+
       //digitalWrite(LED_BUILTIN,HIGH);
       IMU.readSensor(); //Pulls measurements from IMU
       current_X_accel = IMU.getAccelX_mss();
       current_Y_gyro = IMU.getGyroY_rads();
 
-      // Blink the indicator
+      // Blink the green indicator --> shows that IMU is recording data and system is ready for fall detection
       if(currentTime - blinkTimePrior >= blinkDuration){
         if (digitalRead(indicatorGreenPin) == HIGH){
           digitalWrite(indicatorGreenPin, LOW);
@@ -161,17 +170,19 @@ void loop() {
         blinkTimePrior = currentTime;
       }
       
-      // Fill the buffer initially (x-accelerations and y-angular velocity)
-      // Fills buffer full initially --> dont fall in the first 100 Hz...      
+      // Initialization to completely fill the buffer (x-accelerations and y-angular velocity)
       if(!buffer_X_accel.isFull() && !buffer_Y_gyro.isFull()){
         buffer_X_accel.push(current_X_accel);
         buffer_Y_gyro.push(current_Y_gyro);
       }
       
       else{
+
         // Push next sensor reading
         buffer_X_accel.push(current_X_accel);
         buffer_Y_gyro.push(current_Y_gyro);
+
+        // Used for validating values through computer connected testing
         /*
         Serial.print(deltaTime/1000);
         Serial.print(",");
@@ -183,19 +194,20 @@ void loop() {
         */
         
         // Take average of entire buffer to determine current threshold value
-        threshold_X_accel = 0;
-        threshold_Y_gyro = 0;
         using index_t = decltype(buffer_X_accel)::index_t;
         for (index_t i = 0; i < bufferLength; i++){
           threshold_X_accel += buffer_X_accel[i] / bufferLength;
           threshold_Y_gyro += buffer_Y_gyro[i] / bufferLength;
         }
         
-        // Trigger 'fall detection'
-        // Current implementation: output message, flush buffer, set flag off to end measurements, turn on LED and solenoid
-        // Compare current threshold values to preset thresholds (determined through fall testing)
-        if (threshold_X_accel > -4 && threshold_Y_gyro < -1){  //Falls have both of these occuring --> prevent premature triggering due to accel. reading, so use &&
-        // Fall is detected, empty buffers (wont reset unless you turn it off/on) and set flags to false
+        /*
+        1) Current implementation: output message, flush buffer, set flag off to end measurements, turn on LED and solenoid
+        2) If current threshold values exceed preset limit thresholds, fall is occuring --> trigger protective measures
+        3) Falls have both of these occuring --> use two threshold markers to prevent false triggering due to sporadic accel. readings
+        */
+        if (threshold_X_accel > threshold_limit_X_accel && threshold_Y_gyro < threshold_limit_Y_gyro){  
+
+          // Used for computer connected testing
           /*
           Serial.print("Fall Detected! ");
           Serial.print('\n');
@@ -207,37 +219,38 @@ void loop() {
           Serial.print(",");
           Serial.print('\n');
           */
+
+          // Fall is detected --> empty buffers (won't reset unless you turn it off/on) and set flags to false
           buffer_X_accel.clear();
           buffer_Y_gyro.clear();
           sensor_flag = false;
 
           // Turn on actuator and detection LED
-          digitalWrite(actuatorPin, HIGH); //releases CO2 to airbag
-          digitalWrite(indicatorRedPin, HIGH); //indicates you're falling
-          digitalWrite(indicatorGreenPin, LOW); //turn green light off
-          delay(3000); //actuate CO2 flow for 3 seconds to fill airbag to optimal amount
+          digitalWrite(actuatorPin, HIGH); // Releases CO2 to airbag
+          digitalWrite(indicatorRedPin, HIGH); // Indicates that fall is occuring
+          digitalWrite(indicatorGreenPin, LOW); // Turn green light off
+          delay(3000); // Actuate CO2 flow for 3 seconds to fill airbag to optimal amount
           trigger_flag = true;
         }
-
       } 
 
     }
     else{
+
       // Set flag off if timer exceeded and no fall happens
       sensor_flag = false;
       digitalWrite(indicatorGreenPin, HIGH);
     }
   }
   else{
-    //turn actuator off and reset lights
+
+    // Turn actuator off and reset lights
     if (trigger_flag){
       digitalWrite(actuatorPin, LOW);
       digitalWrite(indicatorRedPin, LOW);
       digitalWrite(indicatorGreenPin, HIGH);
       trigger_flag = false;
     }
-    //Turn LED off if flag is off
-    //digitalWrite(actuatorPin, LOW);
     delay(10);
   }
   
